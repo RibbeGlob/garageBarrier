404566b6ee6caba3e720cab359775092fd4ae85e
RibbeGlob <globiszpython@gmail.com>
2024-04-16 18:53:32 +0200

    Added classes for RPI code

--- rpiCode.py
+++ rpiCode.py
@@ -1,39 +1,88 @@
 import RPi.GPIO as GPIO
 import time
-
-GPIO.setmode(GPIO.BCM)
-GPIO_TRIGGER = 18
-GPIO_ECHO = 24
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+import subprocess
 
 
-def distance():
-    GPIO.output(GPIO_TRIGGER, True)
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
-    StartTime, StopTime = time.time(), time.time()
+class Sensor:
+    def __init__(self):
+        self.trigger_sensor, self.echo_sensor = 18, 22
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.trigger_sensor, GPIO.OUT)
+        GPIO.setup(self.echo_sensor, GPIO.IN)
 
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
+    def distance(self):
+        GPIO.output(self.trigger_sensor, True)
+        time.sleep(0.00001)
+        GPIO.output(self.trigger_sensor, False)
+        StartTime, StopTime = time.time(), time.time()
 
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
+        while GPIO.input(self.echo_sensor) == 0:
+            StartTime = time.time()
 
-    TimeElapsed = StopTime - StartTime
-    distance = (TimeElapsed * 34300) / 2
+        while GPIO.input(self.echo_sensor) == 1:
+            StopTime = time.time()
 
-    return distance
+        TimeElapsed = StopTime - StartTime
+        distance = (TimeElapsed * 34300) / 2
+
+        return distance
+
+def capture_image():
+    # Wykonanie polecenia libcamera-still
+    subprocess.run(['libcamera-still', '-o', 'image.jpg'])
+
+
+class Engine:
+    def __init__(self):
+        self.in_one_engine, self.in_two_engine, self.ena_engine = 24, 23, 25
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.in_one_engine, GPIO.OUT)
+        GPIO.setup(self.in_two_engine, GPIO.OUT)
+        GPIO.setup(self.ena_engine, GPIO.OUT)
+        GPIO.output(self.in_one_engine, GPIO.LOW)
+        GPIO.output(self.in_two_engine, GPIO.LOW)
+        self.pwm = GPIO.PWM(self.ena_engine, 1000)
+
+    def forward(self):
+        start_time = time.time()
+
+        while time.time() - start_time < 5:
+            self.pwm.start(50)
+            GPIO.output(self.in_one_engine, GPIO.HIGH)
+            GPIO.output(self.in_two_engine, GPIO.LOW)
+            time.sleep(0.5)
+
+        GPIO.output(self.in_one_engine, GPIO.LOW)
+        GPIO.output(self.in_two_engine, GPIO.LOW)
+        GPIO.cleanup()
+
+    def backward(self):
+        start_time = time.time()
+
+        while time.time() - start_time < 5:
+            self.pwm.start(50)
+            GPIO.output(self.in_one_engine, GPIO.LOW)
+            GPIO.output(self.in_two_engine, GPIO.HIGH)
+            time.sleep(0.5)
+
+        GPIO.output(self.in_one_engine, GPIO.LOW)
+        GPIO.output(self.in_two_engine, GPIO.LOW)
+        GPIO.cleanup()
+
 
 
 if __name__ == '__main__':
     try:
         while True:
-            dist = distance()
-            print("Measured Distance = %.1f cm" % dist)
+            dist = Sensor()
+            distance_value = dist.distance()
+            print("Measured Distance = %.1f cm" % distance_value)
+            if distance_value < 100:
+                print("Obstacle detected close. Taking a picture...")
+                capture_image()
             time.sleep(1)
 
-        # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         print("Measurement stopped by User")
         GPIO.cleanup()
+
